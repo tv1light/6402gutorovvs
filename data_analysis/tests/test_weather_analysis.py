62b10a6816c4b7adeb2ea841235517e3b338d3c4
v.gutorov <v.gutorov@bft.ru>
2024-12-09 01:25:49 +0400

    Lab2#3
    Docstring, some comments

--- data_analysis/tests/test_weather_analysis.py
+++ data_analysis/tests/test_weather_analysis.py
@@ -4,31 +4,59 @@ from data_analysis.weather_analysis.weather_analysis import WeatherDataProcessor
 
 
 class TestWeatherDataProcessor(unittest.TestCase):
+    """
+    Класс для тестирования функциональности WeatherDataProcessor.
+    """
+
     @classmethod
     def setUpClass(cls):
-        # Используем станцию Самары (код 28900)
+        """
+        Метод, выполняющийся перед запуском всех тестов.
+        Инициализирует экземпляр WeatherDataProcessor с заданными параметрами.
+        """
         cls.processor = WeatherDataProcessor(
+            # Используем станцию Самары (код 28900)
             station_id='28900',
             start=datetime(2024, 1, 1),
             end=datetime(2024, 12, 8)
         )
 
     def test_fetch_data(self):
+        """
+        Тестирует загрузку данных.
+        Проверяет, что DataFrame не пустой.
+        """
         self.assertFalse(self.processor.df.empty, "Данные не были загружены")
 
     def test_calculate_moving_average(self):
+        """
+        Тестирует вычисление скользящего среднего.
+        Проверяет наличие колонки 'temp_avg' в DataFrame.
+        """
         df = self.processor.calculate_moving_average(window=3)
         self.assertIn('temp_avg', df.columns, "Отсутствует колонка temp_avg")
 
     def test_compute_diff(self):
+        """
+        Тестирует вычисление дифференциала средней температуры.
+        Проверяет наличие колонки 'temp_diff' в DataFrame.
+        """
         df = self.processor.compute_diff()
         self.assertIn('temp_diff', df.columns, "Отсутствует колонка temp_diff")
 
     def test_find_autocorrelation(self):
+        """
+        Тестирует вычисление автокорреляции.
+        Проверяет, что результат является числом типа float.
+        """
         autocorr = self.processor.find_autocorrelation(lag=1)
         self.assertIsInstance(autocorr, float, "Autocorrelation должно быть числом")
 
     def test_find_extrema(self):
+        """
+        Тестирует поиск экстремумов.
+        Проверяет наличие колонок 'max' и 'min' в DataFrame.
+        """
         df = self.processor.find_extrema()
         self.assertIn('max', df.columns, "Отсутствует колонка max")
         self.assertIn('min', df.columns, "Отсутствует колонка min")
--- data_analysis/weather_analysis/weather_analysis.py
+++ data_analysis/weather_analysis/weather_analysis.py
@@ -4,31 +4,80 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 class WeatherDataProcessor:
+    """
+    Класс для обработки и анализа погодных данных.
+
+    Атрибуты:
+        station_id (str): Идентификатор станции погоды.
+        start (datetime): Начальная дата периода анализа.
+        end (datetime): Конечная дата периода анализа.
+        df (pd.DataFrame): DataFrame с загруженными данными.
+    """
+
     def __init__(self, station_id: str, start: datetime, end: datetime):
+        """
+        Инициализирует WeatherDataProcessor с заданными параметрами.
+
+        :param station_id: Идентификатор станции погоды.
+        :param start: Начальная дата периода анализа.
+        :param end: Конечная дата периода анализа.
+        """
         self.station_id = station_id
         self.start = start
         self.end = end
         self.df = self.fetch_data()
 
     def fetch_data(self) -> pd.DataFrame:
+        """
+         Загружает погодные данные с использованием библиотеки Meteostat.
+
+        :return:
+            pd.DataFrame: DataFrame с загруженными данными.
+        """
         data = Daily(self.station_id, self.start, self.end)
         data = data.fetch()
         data = data.reset_index()
         return data
 
     def calculate_moving_average(self, window: int = 7) -> pd.DataFrame:
+        """
+        Вычисляет скользящее среднее для средней температуры.
+
+        :param window: Размер окна для скользящего среднего. По умолчанию 7.
+        :return:
+            pd.DataFrame: DataFrame с добавленной колонкой 'temp_avg'.
+        """
         self.df['temp_avg'] = self.df['tavg'].rolling(window=window).mean()
         return self.df
 
     def compute_diff(self) -> pd.DataFrame:
+        """
+        Вычисляет разницу (дифференциал) средней температуры.
+
+        :return:
+            pd.DataFrame: DataFrame с добавленной колонкой 'temp_diff'.
+        """
         self.df['temp_diff'] = self.df['tavg'].diff()
         return self.df
 
     def find_autocorrelation(self, lag: int = 1) -> float:
+        """
+        Вычисляет автокорреляцию средней температуры с заданным лагом.
+
+        :param lag: Лаг для автокорреляции. По умолчанию 1.
+        :return:
+            autocorr: Значение автокорреляции.
+        """
         autocorr = self.df['tavg'].autocorr(lag=lag)
         return autocorr
 
     def find_extrema(self) -> pd.DataFrame:
+        """
+        Находит максимумы и минимумы средней температуры.
+
+        :return:
+            pd.DataFrame: DataFrame с добавленными колонками 'max' и 'min'.
+        """
         self.df['max'] = self.df['tavg'][(self.df['tavg'].shift(1) < self.df['tavg']) &
                                          (self.df['tavg'].shift(-1) < self.df['tavg'])]
         self.df['min'] = self.df['tavg'][(self.df['tavg'].shift(1) > self.df['tavg']) &
@@ -36,6 +85,9 @@ class WeatherDataProcessor:
         return self.df
 
     def plot_data(self):
+        """
+        Строит график средней температуры и скользящего среднего.
+        """
         plt.figure(figsize=(12,6))
         plt.plot(self.df['time'], self.df['tavg'], label='Средняя температура')
         plt.plot(self.df['time'], self.df['temp_avg'], label='Скользящее среднее')
@@ -46,4 +98,9 @@ class WeatherDataProcessor:
         plt.show()
 
     def save_to_excel(self, filename: str = 'weather_analysis.xlsx'):
+        """
+        Сохраняет текущий DataFrame в Excel файл.
+
+        :param filename: Имя файла для сохранения. По умолчанию 'weather_analysis.xlsx'.
+        """
         self.df.to_excel(filename, index=False)
